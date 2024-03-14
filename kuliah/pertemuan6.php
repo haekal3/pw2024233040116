f6285103c09a2b61dee00c6c4377392753b57342
Haekal Khadafy <144595822+haekal3@users.noreply.github.com>
2024-03-14 12:40:32 +0700

    pertemuan 6

---
+++ kuliah/pertemuan6.php
@@ -0,0 +1,42 @@
+<?php
+
+$arr = array();
+$kota = array("Bandung","Jakarta");
+
+var_dump($arr);
+echo"<br/>";
+var_dump($kota);
+
+$kota[2] = "Surabaya";
+echo"<br/>";
+var_dump($kota);
+
+$kota[] = "Sukabumi";
+echo"<br/>";
+var_dump($kota);
+echo"<br/>";
+echo"<br/>";
+
+// looping
+for($i=0;$i<count ($kota) ; $i++){
+    echo $kota [$i];
+    echo"<br/>";
+}
+
+echo"<br/>";
+
+foreach($kota as $x){
+    echo $x;
+    echo"<br/>";
+}
+
+echo"<br/>";
+
+foreach($kota as $key):
+    echo $key;
+    echo"<br/>";
+endforeach;
+
+// associative array
+
+?>
